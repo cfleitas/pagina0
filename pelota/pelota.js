08a9be0b5521f7c13b94bb58638bc2832a961341
Carlos Fleitas <cfleitas100@gmail.com>
2025-11-17 21:45:52 +0100

    4

---
+++ pelota/pelota.js
@@ -0,0 +1,36 @@
+let anchoVentana, altoVentana;
+
+let pelota;
+const tamanoPelota = 20;
+let pelotaX, pelotaY;
+let velocidadX = 4;
+let velocidadY = 4;
+
+window.addEventListener('load', iniciarJuego);
+
+function iniciarJuego() {
+    anchoVentana = window.innerWidth;
+    altoVentana = window.innerHeight;
+    pelotaX = (anchoVentana - tamanoPelota) / 2;
+    pelotaY = (altoVentana - tamanoPelota) / 2;
+    pelota = document.getElementById('pelota');
+    pelota.style.width = tamanoPelota + 'px';
+    pelota.style.display = 'block';
+    pelota.style.left = pelotaX + 'px';
+    pelota.style.top = pelotaY + 'px';
+    window.setInterval(moverPelota,1);
+}
+
+function moverPelota() {
+    pelotaX += velocidadX;
+    pelotaY += velocidadY;  
+    if (pelotaX + tamanoPelota >= anchoVentana || pelotaX <= 0) {
+        velocidadX = -velocidadX;
+    }   
+    if (pelotaY + tamanoPelota >= altoVentana || pelotaY <= 0) {
+        velocidadY = -velocidadY;
+    }
+    pelota.style.left = pelotaX + 'px';
+    pelota.style.top = pelotaY + 'px';
+}
+
