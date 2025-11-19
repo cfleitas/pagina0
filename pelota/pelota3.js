c12201a390ce9e93b59f680d0df7e7161ddc3e87
Carlos Fleitas <cfleitas100@gmail.com>
2025-11-19 22:09:38 +0100

    1

---
+++ pelota/pelota3.js
@@ -0,0 +1,41 @@
+let contenedor;
+let anchoContenedor, altoContenedor;
+let contenedorX, contenedorY;
+
+let pelota;
+const tamanioPelota = 25;
+let pelotaX, pelotaY;
+let velocidadX = 1;
+let velocidadY = 1;
+
+window.addEventListener("load", iniciarJuego);
+
+function iniciarJuego() {
+    contenedor = document.getElementById("contenedor");
+    contenedorX = contenedor.getBoundingClientRect().left
+    contenedorY = contenedor.getBoundingClientRect().top;
+    anchoContenedor=contenedor.clientWidth;
+    altoContenedor= contenedor.clientHeight;
+    pelotaX = 200;
+    pelotaY = 200;
+    pelota = document.getElementById("pelota");
+    pelota.style.width=tamanioPelota + 'px';
+    pelota.style.display = "block";
+    pelota.style.left = pelotaX + 'px';
+    pelota.style.top = pelotaY + 'px';
+    window.setInterval(moverPelota, 1000); //lo uso para ver las coordenadas, no me interesa movimiento
+    }
+
+function moverPelota() {
+    pelotaX = 400;
+    pelotaY = 200;
+    pelota.style.left = pelotaX + 'px';
+    pelota.style.top = pelotaY + 'px';
+   // contenedor.innerHTML = "Coordenadas pelotaX: " + pelotaX + " , pelotaY: " + pelotaY ; va bien pero borra la pelota
+   nuevoTexto = document.createElement("p");
+
+    nuevoTexto.innerText = "Coordenadas pelotaX: " + pelotaX + " , pelotaY: " + pelotaY ;
+    contenedor.appendChild(nuevoTexto);
+
+    
+}
